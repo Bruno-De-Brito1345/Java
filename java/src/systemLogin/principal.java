510830b9c01f433bc35f2e13512575096ad5c1f9
Bruno de Brito <brunobrito1345@gmail.com>
2023-11-03 18:48:46 -0300

    Teste incial da hipotese

--- java/src/systemLogin/principal.java
+++ java/src/systemLogin/principal.java
@@ -1,5 +1,32 @@
 package systemLogin;
 
-public class principal {
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.FileFilter;
+import java.io.IOException;
 
+public class principal {
+	/**
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		Scanner leitura = new Scanner(System.in);
+		String sign ="";
+		String user = "Kratimus";
+		String password = "F22sola";
+		//FileWriter escrita = new FileWriter(user + ".txt");
+		
+			System.out.println("Qual o seu login?");
+			sign = leitura.nextLine();
+			
+			if(sign.equals(user)) {
+				System.out.println("Agora Insira sua senha");
+				sign = leitura.nextLine();
+					if(sign.equals(password)) {
+						System.out.println("Login confirmado");
+					}
+					else{System.out.println("Seu login está incorreto");}
+			}
+	}
 }
