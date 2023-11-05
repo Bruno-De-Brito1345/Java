491535d852e3f716afd3b8cd6928dc1a75963ad8
Bruno de Brito <brunobrito1345@gmail.com>
2023-11-04 22:13:26 -0300

    Teste 1: Sucesso

--- java/src/systemLogin/main.java
+++ java/src/systemLogin/main.java
@@ -9,6 +9,28 @@ public class main{
 		
 		Scanner info = new Scanner(System.in);
 		
-		System.out.println();
+		System.out.println("Vamos começar criando sua conta!! Por favor informe seu nome.");
+		
+		Accounts newUser = new Accounts();
+		
+		String dados = info.nextLine();
+		
+		newUser.setName(dados);
+		
+		System.out.println("Prazer em conehçe-lo(a), " + newUser.getName() + 
+				" , agora o senhor poderia informar o seu nome "
+				+ "de usuario e em seguida o seu email? ");
+		
+		dados = info.nextLine();
+		
+		newUser.setUserName(dados);
+		
+		System.out.println("Agora o seu email por favor :>.");
+		
+		dados = info.nextLine();
+		
+		newUser.setEmail(dados);
+		
+		System.out.println(newUser.getName()+ ", "  + newUser.getUserName() + ", " + newUser.getEmail());
 	}
 }
